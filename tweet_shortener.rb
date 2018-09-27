fcfeee9f11ff6358022f4f0c675324a8fdc9e6fc
Maayan Kfir <mayankfir@gmail.com>
2018-09-27 18:32:32 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -38,8 +38,8 @@ end
 
 def shortened_tweet_truncator(tweet)
   if selective_tweet_shortener(tweet).length < 140
-    bulk_tweet_shortener(tweet)
+    tweet
   else
-    tweet[0..136]
+    tweet[0..136] += "..."
   end
 end
